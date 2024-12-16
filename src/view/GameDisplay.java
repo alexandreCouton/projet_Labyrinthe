9b5318a7b7f0d8f6f16777fc68676e4cab590810
acouton <alexandre.couton@etu.unistra.fr>
2024-12-16 09:49:07 +0100

    update liste objectif en changement de joueur

--- src/view/GameDisplay.java
+++ src/view/GameDisplay.java
@@ -171,6 +171,7 @@ public class GameDisplay extends JFrame implements PlateauObserver {
         JButton nextTurnButton = new JButton("Tour Suivant");
         nextTurnButton.addActionListener(e -> {
             m_gameController.nextTurn();
+            updateObjectivePanel();
             m_colorRotation = (m_colorRotation + 1) % 4;
             switch (m_colorRotation) {
                 case 0:
